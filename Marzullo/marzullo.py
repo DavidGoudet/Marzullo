b70c2b49c50785c46b005a5a9043e446cfb92023
David Goudet <davidgccs@hotmail.com>
2015-01-28 20:09:16 -0430

    David: algoritmo escrito

--- Marzullo/marzullo.py
+++ Marzullo/marzullo.py
@@ -3,7 +3,7 @@ def marzullo_algorithm(ranges):
     for l,r in ranges:
         table.append((l,-1))
         table.append((r,+1))
-    
+    print(table)
     for i in range( len( table ) ):
         for k in range( len( table ) - 1, i, -1 ):
             if ( table[k][0] < table[k - 1][0] ):
@@ -11,9 +11,17 @@ def marzullo_algorithm(ranges):
             if ( table[k][0] == table[k - 1][0] ):
                 if ( table[k][1] < table[k - 1][1] ):
                     swap ( table, k, k - 1 )
-                    
-    print(table)
-    return (11,12)
+    print(table)         
+    best = 0
+    cnt = 0
+    for i in range(len(table) - 1):
+        cnt = cnt - table[i][1]
+        if cnt > best:
+            best = cnt
+            beststart = table[i][0]
+            bestend   = table[i+1][0]
+    return (beststart, bestend)                
+    
    
 def swap( A, x, y ):
     tmp = A[x]
@@ -31,6 +39,11 @@ def test(data_list):
  
 if __name__ == "__main__":
     test_data_list = (
-            {'input' : ((8,12),(11,13),(10,12),(12,15),(13,14)), 'expected' : (11,12)},
-            )
+        {'input' : ((8,12),(11,13),(10,12),(12,15),(13,14)), 'expected' : (11,12)},
+        {'input' : ((8,12),(11,13),(14,15)), 'expected' : (11,12)},
+        {'input' : ((8,9),(8,12),(10,12)), 'expected' : (8,9)},
+        {'input' : ((8,12),(9,10),(11,13),(10,12)), 'expected' : (11,12)},
+        {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (9,10)},
+        {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)})
+            
     test(test_data_list)
