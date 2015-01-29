62b3a5a0bba7ce5783880a2e2c55c480206cd717
jh_sanchez <jhsanchezz@gmail.com>
2015-01-28 21:03:34 -0430

    Corregido error tras corrida en frio

--- Marzullo/marzullo.py
+++ Marzullo/marzullo.py
@@ -9,7 +9,7 @@ def marzullo_algorithm(ranges):
             if ( table[k][0] < table[k - 1][0] ):
                 swap( table, k, k - 1 )
             if ( table[k][0] == table[k - 1][0] ):
-                if ( table[k][1] < table[k - 1][1] ):
+                if ( table[k][1] > table[k - 1][1] ):
                     swap ( table, k, k - 1 )
     print(table)         
     best = 0
@@ -39,11 +39,6 @@ def test(data_list):
  
 if __name__ == "__main__":
     test_data_list = (
-        {'input' : ((8,12),(11,13),(10,12),(12,15),(13,14)), 'expected' : (11,12)},
-        {'input' : ((8,12),(11,13),(14,15)), 'expected' : (11,12)},
-        {'input' : ((8,9),(8,12),(10,12)), 'expected' : (8,9)},
-        {'input' : ((8,12),(9,10),(11,13),(10,12)), 'expected' : (11,12)},
-        {'input' : ((8,12),(9,10),(11,13),(14,15)), 'expected' : (9,10)},
-        {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)})
+            {'input' : ((11,15),(8,15),(9,11),(10,14),(11,14),(9,10),(9,13),(12,15),(8,11),(14,15)), 'expected' : (12,13)},)
             
     test(test_data_list)
