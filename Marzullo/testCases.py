a18b7d6dea1dc09e4f45b1bbca0d69ac7ee2adc7
jh_sanchez <jhsanchezz@gmail.com>
2015-01-29 19:40:26 -0430

    Primeras pruebas y arreglo de errores

--- Marzullo/testCases.py
+++ Marzullo/testCases.py
@@ -4,15 +4,40 @@ import unittest
 class testMarzullo(unittest.TestCase):
 
         
-    def testNingunaEntrada(self):
-        self.assertEqual( marzullo_algorithm(()), (0,0) )
+    def testNoEntry(self):
+        self.assertTrue( marzullo_algorithm(()) )
         
-    def testMaximoDePuestos(self):
+    def testMaxParking(self):
         
-        self.assertEqual( marzullo_algorithm( ( 
-                                               (8,12), (11,13), (10,12), (9,12), (7,12), (10,12), (7,12),
-                                               (9,15), (10,18), (11, 14)
-                                               ) ), (11,12) )
+        self.assertTrue( marzullo_algorithm( ( 
+                                                (8,12), (11,13), (10,12), (9,12), (7,12), (10,12), (7,12),
+                                                (9,15), (10,18), (11, 14)
+                                                ) )  )
+        
+    def testMaxHourMinHour(self):
+        
+        self.assertTrue( marzullo_algorithm( ( 
+                                                (6,18),
+                                                ) )  )
+        
+    def testStartEqualToEnd(self):
+        
+        self.assertTrue( marzullo_algorithm( ( 
+                                                (15,15),
+                                                ) )  )
+        
+    def testFullParking(self):
+        
+        self.assertFalse( marzullo_algorithm( ( 
+                                                (8,12), (11,13), (10,12), (9,12), (7,12), (10,12), (7,12),
+                                                (9,15), (10,18), (11, 14), (9,13)
+                                                ) )  )
+        
+    def testStartAfterEnd(self):
+        
+        self.assertFalse( marzullo_algorithm( ( 
+                                                (15,12),
+                                                ) )  )
  
-    if __name__ == "__main__":
-        unittest.main()
+if __name__ == "__main__":
+    unittest.main()
